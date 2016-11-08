e0c780e9f9014e76f4fe6be86e30c8eed3c456c3
nicolasblancorequejo <nicolas.blancorequejo@gmail.com>
2016-11-08 17:19:11 +0100

    Added ContentProviders oef

--- AndroidStudioProjects/10_RessourcesData/app/src/main/java/com/example/delta/a10_ressourcesdata/MainActivity.java
+++ AndroidStudioProjects/10_RessourcesData/app/src/main/java/com/example/delta/a10_ressourcesdata/MainActivity.java
@@ -1,96 +1,59 @@
 package com.example.delta.a10_ressourcesdata;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.provider.Settings;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    public static final String PREFS_NAME = "MyPrefsFile";
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] mProjection = {
+                Settings.System._ID,
+                Settings.System.NAME,
+                Settings.System.VALUE
+        };
+        String mSelectionClause = null; // Defines a string to contain the selection clause
+        String[] mSelectionArgs = null; // Initializes an array to contain selection arguments
+        String mOrderBy = null;
 
-        Button btn_register = (Button)findViewById(R.id.login_btn_register);
-        btn_register.setOnClickListener(this);
-
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String username = settings.getString("loginUsername", "");
-        EditText txt_username = (EditText)findViewById(R.id.login_txt_username);
-        txt_username.setText(username);
-
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        EditText txt_username = (EditText)findViewById(R.id.login_txt_username);
-        EditText txt_password = (EditText)findViewById(R.id.login_txt_password);
-
-        String txt_U = txt_username.getText().toString();
-        String txt_P = txt_password.getText().toString();
-
-        Context context = getApplicationContext();
-        CharSequence text = "De registratie is gelukt!";
-        int duration = Toast.LENGTH_SHORT;
-
-        if (txt_U.length() < 5){
-            text = "Uw naam moet minstens 5 karakters lang zijn!";
-        }
-
-        if (txt_P.length() < 5){
-            text = "Uw wachtwoord moet minstens 5 karakters lang zijn!";
-        }
-
-        if (!hasOneDigitAndOneLetter(txt_P)){
-            text = "Uw wachtwoord moet minstens 1 letter en  1 cijfer bevatten!";
-        }
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-    }
-
-    public static boolean hasOneDigitAndOneLetter(String pass)
-    {
-        boolean hasLetter = false;
-        boolean hasDigit = false;
-        for(int i=0; i<pass.length(); i++)
-        {
-            if(Character.isLetter((pass.charAt(i))))
-            {
-                hasLetter = true;
+        try {
+            Cursor mCursor = getContentResolver().query(
+                    Settings.System.CONTENT_URI,  // The content URI of the words table
+                    mProjection,                       // The columns to return for each row
+                    mSelectionClause,                   // Either null, or the word the user entered
+                    mSelectionArgs,                    // Either empty, or the string the user entered
+                    mOrderBy);
+            if (mCursor == null){
+                throw new Exception("error");
+            } else if (mCursor.getCount()<1){
+                Toast.makeText(getApplicationContext(), "Unsuccesful search", Toast.LENGTH_LONG).show();
+            } else{
+                String[] mWordListColumns =
+                        {
+                                Settings.System.NAME,
+                                Settings.System.VALUE
+                        };
+                int[] mWordListItems = { R.id.list_naam, R.id.list_waarde};
+                SimpleCursorAdapter mCursorAdapter = new SimpleCursorAdapter(
+                        getApplicationContext(),
+                        R.layout.simple_list_item_1,
+                        mCursor,
+                        mWordListColumns,
+                        mWordListItems,
+                        0);
+                ListView listView = (ListView)findViewById(R.id.listview);
+                listView.setAdapter(mCursorAdapter);
             }
-            if(Character.isDigit((pass.charAt(i))))
-            {
-                hasDigit = true;
-            }
-
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        return hasLetter&&hasDigit;
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-
-        EditText txt_username = (EditText)findViewById(R.id.login_txt_username);
-        String username = txt_username.getText().toString();
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("loginUsername", username);
-
-        // Commit the edits!
-        editor.commit();
     }
 }
